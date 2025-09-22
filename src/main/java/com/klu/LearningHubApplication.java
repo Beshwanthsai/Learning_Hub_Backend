7cb491d66162b85cfe8cb269332e7e96c59a2da5
Beshwanth Sai Katari <143738062+Beshwanthsai@users.noreply.github.com>
2025-09-22 18:59:38 +0530

    Update LearningHubApplication.java

--- src/main/java/com/klu/LearningHubApplication.java
+++ src/main/java/com/klu/LearningHubApplication.java
@@ -1,3 +1,10 @@
+package com.klu; // make sure this matches your folder structure
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
 public class LearningHubApplication extends SpringBootServletInitializer {
 
