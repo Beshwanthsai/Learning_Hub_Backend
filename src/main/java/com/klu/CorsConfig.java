2a99f4bc89d52709a3ee6b7f6d2a80f4c80bf1ef
Beshwanth Sai Katari <143738062+Beshwanthsai@users.noreply.github.com>
2025-09-22 20:54:45 +0530

    Update allowed origins in CORS configuration

--- src/main/java/com/klu/CorsConfig.java
+++ src/main/java/com/klu/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://13.220.125.179:9090")
+                        .allowedOrigins("http://54.221.128.237:9090")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
