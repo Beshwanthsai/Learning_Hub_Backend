3ced21aca86d4ec28b69cc5f76e383a20b113efe
Beshwanth Sai Katari <143738062+Beshwanthsai@users.noreply.github.com>
2025-09-22 11:30:44 +0530

    Update CorsConfig.java

--- src/main/java/com/klu/CorsConfig.java
+++ src/main/java/com/klu/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://98.84.153.128:9090")
+                        .allowedOrigins("http://3.95.215.66:9090")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
